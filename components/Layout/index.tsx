c5b667655c9d24fc8bc3173b8b28693437d12809
Alireza8080-debug <AlirezaShahmoradi98@gmail.com>
2022-05-30 01:03:24 +0430

    added sign in & sign up mui template

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,11 +1,10 @@
 import { FC, ReactNode } from 'react';
 import Footer from '../Footer';
 import Navbar from '../Navbar';
-import classes from './index.module.scss';
 
 const Layout: FC<{ children: ReactNode }> = ({ children }) => {
   return (
-    <div className={classes.body}>
+    <div>
       <Navbar />
       {children}
       <Footer />
