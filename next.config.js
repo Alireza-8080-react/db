09f10480259184731376636bac1aec6e0884a532
Alireza8080-debug <AlirezaShahmoradi98@gmail.com>
2022-05-29 23:39:51 +0430

    removed scss from next config

--- next.config.js
+++ next.config.js
@@ -4,14 +4,3 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
-
-const path = require('path');
-const withSass = require('@zeit/next-sass');
-module.exports = withSass({
-  cssModules: true
-});
-module.exports = {
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')]
-  }
-};
