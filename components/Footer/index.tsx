d7e5e2e9a1f2486110a6bb1656e94478df7a85d8
Alireza8080-debug <AlirezaShahmoradi98@gmail.com>
2022-05-30 01:21:25 +0430

    removed Footer content

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,17 +1,7 @@
-import { Link, Typography } from '@mui/material';
 import { FC } from 'react';
 
 const Footer: FC = () => {
-  return (
-    <Typography variant='body2' color='text.secondary' align='center'>
-      {'Copyright © '}
-      <Link color='inherit' href='https://mui.com/'>
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
+  return <></>;
 };
 
 export default Footer;
