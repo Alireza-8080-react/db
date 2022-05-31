22640c70c22cc1f256967839a693022ea7c6c466
Alireza8080-debug <AlirezaShahmoradi98@gmail.com>
2022-06-01 02:22:08 +0430

    changed sign in page to login

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,12 +1,7 @@
 import * as React from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
@@ -18,11 +13,11 @@ export default function SignIn() {
 
   return (
     <>
-      <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+      <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }} className='body'>
         <LockOutlinedIcon />
       </Avatar>
       <Typography component='h1' variant='h5'>
-        Sign in
+        ورود
       </Typography>
       <Box component='form' onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
         <TextField
@@ -30,7 +25,7 @@ export default function SignIn() {
           required
           fullWidth
           id='email'
-          label='Email Address'
+          label='آدرس پست الکترونیک'
           name='email'
           autoComplete='email'
           autoFocus
@@ -40,30 +35,14 @@ export default function SignIn() {
           required
           fullWidth
           name='password'
-          label='Password'
+          label='رمز عبور'
           type='password'
           id='password'
           autoComplete='current-password'
         />
-        <FormControlLabel
-          control={<Checkbox value='remember' color='primary' />}
-          label='Remember me'
-        />
         <Button type='submit' fullWidth variant='contained' sx={{ mt: 3, mb: 2 }}>
-          Sign In
+          ورود
         </Button>
-        <Grid container>
-          <Grid item xs>
-            <Link href='#' variant='body2'>
-              Forgot password?
-            </Link>
-          </Grid>
-          <Grid item>
-            <Link href='/sign-up' variant='body2'>
-              {"Don't have an account? Sign Up"}
-            </Link>
-          </Grid>
-        </Grid>
       </Box>
     </>
   );
