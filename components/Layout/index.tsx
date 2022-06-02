f4bff703230eaaf875db6c362ec2d5fcf15105f4
Alireza8080-debug <AlirezaShahmoradi98@gmail.com>
2022-06-02 22:53:32 +0430

    added ToastContainer to dom tree

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,12 +1,15 @@
 import { CacheProvider } from '@emotion/react';
 import { Box, Container, CssBaseline } from '@mui/material';
 import { FC, ReactNode } from 'react';
+import { ToastContainer } from 'react-toastify';
 import Navbar from '../Navbar';
 import { cacheRtl } from './theme';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Layout: FC<{ children: ReactNode }> = ({ children }) => {
   return (
     <CacheProvider value={cacheRtl}>
+      <ToastContainer />
       <Navbar />
       <Container component='main' maxWidth='xs'>
         <CssBaseline />
